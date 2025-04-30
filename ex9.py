37f6a2c1e03240af8b1e88560a6a79e6db6e7567
Vilhena123 <kevenvilhena2003@gmail.com>
2025-04-29 21:27:43 -0300

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,12 @@
+#9 - escreva um software que some dois números.
+# soma de dois numeros 
+
+#solicita os numerosa ao usuario
+num1 = float(input("digite o primeiro numero:"))
+num2 = float(input("digite o segundo numero:"))
+
+#soma 
+soma = num1 + num2
+
+#imprime resultado
+print("a soma dos dois numeros é:", soma)
